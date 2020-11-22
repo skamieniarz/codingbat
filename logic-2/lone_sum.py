d1fd3a08d2995a2da65bf16bae4529f4fee36cb2
skamieniarz <46318321+skamieniarz@users.noreply.github.com>
2020-11-22 09:47:17 +0000

    Update lone_sum.py

--- logic-2/lone_sum.py
+++ logic-2/lone_sum.py
@@ -3,11 +3,9 @@ is the same as another of the values, it does not count towards the sum. '''
 
 
 def lone_sum(a, b, c):
-    ints_sum = 0
-    if a not in [b, c]:
-        ints_sum += a
-    if b not in [a, c]:
-        ints_sum += b
-    if c not in [a, b]:
-        ints_sum += c
-    return ints_sum
+  lone_sum = 0
+  ints_list = [a, b, c]
+  for item in ints_list:
+    if ints_list.count(item) == 1:
+      lone_sum += item
+  return lone_sum
